1487ca85117058a4d40f8c606401af0e372e3189
Filip Geib <filip9geib@gmail.com>
2019-08-26 14:21:17 +0200

    changed to functions

--- pythonScripts/boundaries2laps.py
+++ pythonScripts/boundaries2laps.py
@@ -5,98 +5,107 @@ import argparse
 import csv
 
 
+# FUNCTIONS ------------------------------------------------------------------------------------------------------------
+
+def boundaries2laps(pathBound, pathLaps):
+    # check if boundaries file exists and is readable
+    checkAccess(pathBound, 'r')
+
+    # load and check boundaries file
+    boundFile = open(pathBound, 'r')
+    checkOpen(boundFile, 'r')
+
+    # create boundaries CSV reader
+    reader = csv.reader(boundFile, delimiter=',', lineterminator='\n')
+
+    # create and check laps file
+    lapsFile = open(pathLaps, 'w')
+    checkOpen(lapsFile, 'w')
+
+    # create laps CSV writer and write header
+    writer = csv.writer(lapsFile, delimiter=',', lineterminator='\n')
+    writer.writerow(['LapNo', 'Start', 'End', 'Duration'])
+
+    # PROCESS BOUNDARIES FILE AND CREATE LAPS FILE ---------------------------------------------------------------------
+
+    # skip header row
+    next(reader)
+
+    # current lap counter, last boundary, and variables for time checking
+    currLap = 1
+    prevEnd = None
+    currStart = None
+
+    # check if first boundary data is static, pre-read row for next loop
+    row = next(reader)
+    lastBound = str2float(row[1])
+    if not lastBound == 1: outputHandler('corrupted boundaries file - starts with 0', han.err)
+
+    # loop thru whole boundaries file
+    for row in reader:
+
+        # retype row from string to float
+        row[0] = str2float(row[0])
+        row[1] = str2float(row[1])
+
+        # detect falling edge => lap start
+        if lastBound == 1 and row[1] == 0:
+            currStart = row[0]
+
+        # detect rising edge => lap end and proceed current lap
+        elif lastBound == 0 and row[1] == 1:
+            currEnd = row[0]
+
+            # check if lap times make sense
+            if not currStart < currEnd:
+                outputHandler('start time !< than end time in lap ' + str(currLap), han.err)
+
+            # check if lap time is after previous lap
+            if prevEnd is not None and currStart < prevEnd:
+                outputHandler('end of previous lap !< than start of current lap ' + str(currLap), han.err)
+
+            # compute current lap duration
+            currDuration = round(abs(currStart - currEnd), 2)
+
+            # everything is OK, write to CSV
+            writer.writerow([currLap, currStart, currEnd, currDuration])
+
+            # update variables
+            currLap += 1
+            prevEnd = currEnd
+
+        # update lastBound
+        lastBound = row[1]
+
+    # check if last boundary data is static
+    if not lastBound == 1: outputHandler('corrupted boundaries file - ends with 0', han.err)
+
+    # close open CSV files
+    boundFile.close()
+    lapsFile.close()
+
+    # return success
+    return True
+
+
 # MAIN------------------------------------------------------------------------------------------------------------------
+if __name__ == "__main__":
 
-# FILES HANDLING -------------------------------------------------------------------------------------------------------
+    # create input arguments parser
+    parser = argparse.ArgumentParser(description='Parse individual laps from binary boundaries file.')
 
-# create input arguments parser
-parser = argparse.ArgumentParser(description='Parse individual laps from binary boundaries file.')
+    # add required arguments
+    parser.add_argument('-b', '--bound', help='path to existing CSV boundary file', type=str, required=True)
+    parser.add_argument('-l', '--laps', help='path to future CSV laps file', type=str, required=True)
 
-# add required arguments
-parser.add_argument('-b', '--bound', help='path to existing CSV boundary file', type=str, required=True)
-parser.add_argument('-l', '--laps', help='path to future CSV laps file', type=str, required=True)
+    # load input arguments
+    arguments = parser.parse_args()
 
-# load input arguments
-arguments = parser.parse_args()
+    # compute directories
+    pathBoundPy = Path(arguments.bound)
+    pathLapsPy = Path(arguments.laps)
 
-# compute directories
-pathBound = Path(arguments.bound)
-pathLaps = Path(arguments.laps)
-
-# check if boundaries file exists and is readable
-checkAccess(pathBound, 'r')
-
-# load and check boundaries file
-boundFile = open(pathBound, 'r')
-checkOpen(boundFile, 'r')
-
-# create boundaries CSV reader
-reader = csv.reader(boundFile, delimiter=',', lineterminator='\n')
-
-# create and check laps file
-lapsFile = open(pathLaps, 'w')
-checkOpen(lapsFile, 'w')
-
-# create laps CSV writer and write header
-writer = csv.writer(lapsFile, delimiter=',', lineterminator='\n')
-writer.writerow(['LapNo', 'Start', 'End', 'Duration'])
-
-# PROCESS BOUNDARIES FILE AND CREATE LAPS FILE -------------------------------------------------------------------------
-
-# skip header row
-next(reader)
-
-# current lap counter, last boundary, and variables for time checking
-currLap = 1
-prevEnd = None
-currStart = None
-
-# check if first boundary data is static, pre-read row for next loop
-row = next(reader)
-lastBound = str2float(row[1])
-if not lastBound == 1: outputHandler('corrupted boundaries file - starts with 0', han.err)
-
-# loop thru whole boundaries file
-for row in reader:
-
-    # retype row from string to float
-    row[0] = str2float(row[0])
-    row[1] = str2float(row[1])
-
-    # detect falling edge => lap start
-    if lastBound == 1 and row[1] == 0:
-        currStart = row[0]
-
-    # detect rising edge => lap end and proceed current lap
-    elif lastBound == 0 and row[1] == 1:
-        currEnd = row[0]
-
-        # check if lap times make sense
-        if not currStart < currEnd:
-            outputHandler('start time !< than end time in lap ' + str(currLap), han.err)
-
-        # check if lap time is after previous lap
-        if prevEnd is not None and currStart < prevEnd:
-            outputHandler('end of previous lap !< than start of current lap ' + str(currLap), han.err)
-
-        # compute current lap duration
-        currDuration = round(abs(currStart - currEnd), 2)
-
-        # everything is OK, write to CSV
-        writer.writerow([currLap, currStart, currEnd, currDuration])
-
-        # update variables
-        currLap += 1
-        prevEnd = currEnd
-
-    # update lastBound
-    lastBound = row[1]
-
-# check if last boundary data is static
-if not lastBound == 1: outputHandler('corrupted boundaries file - ends with 0', han.err)
-
-# close open CSV files
-boundFile.close()
-lapsFile.close()
+    # call function
+    boundaries2laps(pathBoundPy, pathLapsPy)
 
 # CODE END -------------------------------------------------------------------------------------------------------------
---
+++ pythonScripts/dataCSVsplitting.py
@@ -0,0 +1,181 @@
+from functions import *
+from pathlib import Path
+
+import argparse
+import csv
+import time
+
+
+# FUNCTIONS ------------------------------------------------------------------------------------------------------------
+def dataCSVsplitting(pathData, pathLaps, dirFinal, sampleRate):
+    # compute period
+    period = sampleRate / 1
+
+    # check if files exists and are readable
+    checkAccess(pathData, 'r')
+    checkAccess(pathLaps, 'r')
+
+    # check if directory for separated laps exists and is writable
+    checkAccess(Path(dirFinal), 'w')
+
+    # LOAD START AND END TIMES OF EACH LAP -----------------------------------------------------------------------------
+
+    # open laps csv file
+    lapsFile = open(pathLaps, 'r')
+    checkOpen(lapsFile, 'r')
+
+    # create csv reader
+    reader = csv.reader(lapsFile, delimiter=',')
+
+    # preallocate arrays for starting and ending times of laps
+    # noinspection PyUnusedLocal
+    lapsSum = sum(1 for row in lapsFile) - 1
+    lapStart = lapsSum * [float('nan')]
+    lapEnd = lapsSum * [float('nan')]
+
+    # reset reader and skip header for new loop
+    lapsFile.seek(0)
+    next(reader)
+
+    # read laps csv file line by line and store values to arrays declared above
+    for row in reader:
+        # process lap number
+        lapNo = 1
+        try:
+            lapNo = int(row[0])
+        except ValueError:
+            outputHandler("corrupted laps CSV file, col: 1", han.err, lapNo)
+
+        # process lap start time
+        try:
+            lapStart[lapNo - 1] = float(row[1])
+        except ValueError:
+            outputHandler("corrupted laps CSV file, col: 2", han.err, lapNo)
+
+        # process lap end time
+        try:
+            lapEnd[lapNo - 1] = float(row[2])
+        except ValueError:
+            outputHandler("corrupted laps CSV file, col: 3", han.err, lapNo)
+
+        # just for case of future error
+        lapNo += 1
+
+    # close laps csv file
+    lapsFile.close()
+
+    # change starting and ending times of laps to starting and ending lines
+    for i in range(len(lapStart)):
+        lapStart[i] = round(lapStart[i] * sampleRate)
+        lapEnd[i] = round(lapEnd[i] * sampleRate)
+
+    # inform about current state
+    outputHandler("all files loaded successfully, starting to split CSV", han.info)
+
+    # SPLIT INPUT CSV FILE TO CHUNKS (LAPS) ACCORDING TO LOADED LAPS TIMES ---------------------------------------------
+
+    # open data CSV file
+    dataFile = open(pathData, 'r')
+    checkOpen(dataFile, 'r')
+
+    # create csv reader, lap counter, lap time and execution time
+    reader = csv.reader(dataFile, delimiter=',')
+    currentLap = 1
+    timeLap = 0
+    lastExecTime = time.time()
+
+    # load original header
+    header = next(reader)
+
+    # create starting path to very first csv output
+    currentPath = Path(dirFinal + "/IMU_lap_" + str(currentLap).zfill(2) + ".csv")
+
+    # create csv file for currently processed lap
+    currentFile = open(currentPath, 'w')
+    checkOpen(currentFile, 'w')
+
+    # create writer for currently processed lap
+    currentWriter = csv.writer(currentFile, delimiter=',', lineterminator='\n')
+
+    # write header to current CSV file
+    currentWriter.writerow(header)
+
+    for i, row in enumerate(reader):
+        if lapStart[currentLap - 1] <= i <= lapEnd[currentLap - 1]:
+            # change absolute time to lap time
+            row[0] = str(timeLap)
+
+            # write row to lap file
+            currentWriter.writerow(row)
+
+            # compute next lap time
+            timeLap += period
+
+            if i == lapEnd[currentLap - 1]:
+                # close actual csv file
+                currentFile.close()
+
+                # inform user
+                outputHandler('lap no. ' + str(currentLap) + ' splitted in: ' +
+                              timeDeltaStr(time.time(), lastExecTime), han.info)
+
+                # update lap and time
+                currentLap += 1
+                lastExecTime = time.time()
+
+                # reset internal lap time
+                timeLap = 0
+
+                if currentLap <= len(lapStart):
+                    # create starting path to next  csv output
+                    currentPath = Path(dirFinal + "/IMU_lap_" + str(currentLap).zfill(2) + ".csv")
+
+                    # create csv file for currently processed lap
+                    currentFile = open(currentPath, 'w')
+                    checkOpen(currentFile, 'w')
+
+                    # create writer for currently processed lap
+                    currentWriter = csv.writer(open(currentPath, 'w'), delimiter=',', lineterminator='\n')
+
+                    # write header to current CSV file
+                    currentWriter.writerow(header)
+
+                # no more laps expected
+                else:
+                    break
+
+    # close files
+    dataFile.close()
+    currentFile.close()
+
+    # return success
+    return True
+
+
+# MAIN------------------------------------------------------------------------------------------------------------------
+if __name__ == "__main__":
+
+    # create input arguments parser
+    parser = argparse.ArgumentParser(description='Parse and check raw IMU and GPS files to CSV format.')
+
+    # add required arguments
+    parser.add_argument('-d', '--data', help='path to data file', type=str, required=True)
+    parser.add_argument('-l', '--laps', help='path to laps file', type=str, required=True)
+    parser.add_argument('-f', '--final', help='path to final data folder', type=str, required=True)
+    parser.add_argument('-s', '--sampleRate', help='sample rate of data file', type=int, required=True)
+
+    # load input arguments
+    arguments = parser.parse_args()
+
+    # load sample rate
+    sampleRatePy = arguments.sampleRate
+
+    # compute directories
+    pathDataPy = Path(arguments.data)
+    pathLapsPy = Path(arguments.laps)
+    dirFinalPy = arguments.final
+
+    # call function
+    dataCSVsplitting(pathDataPy, pathLapsPy, dirFinalPy, sampleRatePy)
+
+# CODE END -------------------------------------------------------------------------------------------------------------
