5f8be531584ae7de9c1add42f0426953082cbe04
Filip Geib <filip9geib@gmail.com>
2019-08-27 12:00:09 +0200

    gravity from config

--- pythonScripts/dataParsing.py
+++ pythonScripts/dataParsing.py
@@ -3,14 +3,12 @@ from pathlib import Path
 
 import argparse
 import math
-import os
 import time
 import csv
 
 
 # DEFINITIONS ----------------------------------------------------------------------------------------------------------
 
-GRAVITY = 9.80581295200000
 DECIMAL = 6
 DELTAGPSUTC = 315964782  # current update for data after Jan 1 2017
 
@@ -41,6 +39,7 @@ class path:
 
 
 class conf:
+    gravity = None
     sampleRate = None
 
 
@@ -150,10 +149,13 @@ checkAccess(path.experiment, 'w')
 
 # LOAD DATA FROM CONFIG FILE AND RENAME FILES --------------------------------------------------------------------------
 
-# load sample rate from config file
+# load gravity and sample rate from config file
+conf.gravity = readConfig(path.config, 'gravity')
 conf.sampleRate = readConfig(path.config, 'sample_rate')
 
 # check if is valid
+if math.isnan(conf.gravity):
+    outputHandler('loaded gravity is NaN value', han.err)
 if math.isnan(conf.sampleRate):
     outputHandler('loaded sample rate is NaN value', han.err)
 
@@ -355,7 +357,7 @@ for lineCnt, line in enumerate(IMUtmpFile, start=1):
 
     # accelerometer - divide with gravity constant
     for i in range(3):
-        dataOut[1 + i] = line[2 + i] / GRAVITY
+        dataOut[1 + i] = line[2 + i] / conf.gravity
 
     # gyroscope - nothing to do
     for i in range(3):
