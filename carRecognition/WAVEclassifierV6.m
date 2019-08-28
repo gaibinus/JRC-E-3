446b03bf60e03bbdc634ffb9f9373fb6b3c55f70
Filip Geib <filip9geib@gmail.com>
2019-08-28 19:30:59 +0200

    Create WAVEclassifierV6.m

---
+++ carRecognition/WAVEclassifierV6.m
@@ -0,0 +1,127 @@
+
+%% FILE MANAGEMENT
+
+close all; clear; clc;
+
+% load experiment data structure as data
+load('C:\Users\geibfil\Desktop\JRC-E-3\experiments\data_10.mat', 'data');
+
+% save constants
+SEGMENT_SIZE = 20; % in [s]
+SAMPLE_RATE = 10; % in [Hz]
+CARS = size(data, 1);
+
+%% COMPUTE AND PLOT VARIANCE
+
+% compute increase between segments
+segIncrease = SEGMENT_SIZE * SAMPLE_RATE;
+
+% find longest lap from whole data set
+maxLength = max(max(cellfun(@height,data,'UniformOutput',true)));
+
+% compute number of segments
+segCount = ceil(maxLength / segIncrease);
+
+% prealloc results cell array as: results{car, method}
+results = cell(CARS, 8);
+
+for car = 1 : CARS
+    % create table header as cell array
+    header = strsplit(sprintf('Car%dLap%d,', [car*ones(1,20); 1:20]), ',');
+    header(cellfun('isempty',header)) = [];
+    
+    % create tables of every method for current car
+    for method = 1 : 8
+        results{car, method} = array2table(zeros(segCount, 20), ...
+            'VariableNames', header);
+    end
+       
+    for segment = 1 : segCount
+        % compute end of current segment 
+        segEnd = segIncrease * segment;
+        
+        % prealloc tmp results as: tmpResults{method, lap}
+        tmpResults = NaN(8, 20);
+              
+        for lap = 1 : 20    
+            % check if segEnd overflov current lap
+            if segEnd > size(data{car, lap}, 1)
+                continue;
+            end
+            
+            % load AccZ data from current lap in lenght of current segment
+            tmpData = data{car, lap}{1:segEnd, 'AccZ'};
+
+            tmpResults(1, lap) = var(tmpData);
+            tmpResults(2, lap) = wentropy(tmpData, 'shannon');
+            tmpResults(3, lap) = skewness(tmpData);
+            tmpResults(4, lap) = kurtosis(tmpData);
+
+            % load GyrY data from current lap in lenght of current segment
+            tmpData = data{car, lap}{1:segEnd, 'GyrY'};
+
+            tmpResults(5, lap) = var(tmpData);
+            tmpResults(6, lap) = wentropy(tmpData, 'shannon');
+            tmpResults(7, lap) = skewness(tmpData);
+            tmpResults(8, lap) = kurtosis(tmpData);
+        end
+        
+        % add current segment to results
+        for method = 1 : 8
+            results{car, method}{segment, :} = tmpResults(method,:);            
+        end
+        
+    end
+end
+
+% create array with method names and data names
+methodNames = ["variance", "wentropy", "skewness", "kurtosis"];
+dataNames = ["acceleration on Z axis", "gyroscope on Y axis"];
+
+%% PLOT VARIANCE RESULTS FOR EVERY METHODE
+
+for method = 1 : 1
+
+   % compute methode and data name strings
+   methodStr = methodNames(rem(5-1, 4) + 1);
+   dataStr = dataNames(floor((method-1)/4) + 1);
+
+   % create figure and set it up
+   fig = figure();
+   fig.Name = strcat(methodStr, ' of ', dataStr);
+   
+   % join all cars to one table
+   plotResults = table();
+   for car = 1 : CARS
+       plotResults = [plotResults, results{car, method}];
+   end
+   
+   % create display variables parameter
+   dispVar = cell(20,1);
+   for lap = 1 : 20
+       tmp = strsplit(sprintf('Car%dLap%d\n', [1:CARS; ...
+                                               lap * ones(1,CARS)]));
+       tmp(cellfun('isempty',tmp)) = [];
+       dispVar{lap,:} = tmp;
+   end
+   
+   % create display labels parameter
+   dispLab = strsplit(sprintf('Lap %d,', 1:20), ',');
+   dispLab(cellfun('isempty',dispLab)) = [];
+   
+   % create legend labels parameter
+   legLab = strsplit(sprintf('Car %d,', 1:CARS), ',');
+   legLab(cellfun('isempty',legLab)) = [];
+   
+   % create stacked plot and set it up
+   statPlot = stackedplot(plotResults, dispVar);
+   statPlot.DisplayLabels = dispLab;
+   statPlot.XLabel = 'Segment ID';
+   statPlot.AxesProperties(1).LegendLabels = legLab;
+   statPlot.AxesProperties(1).LegendVisible = 'on';
+   
+   for i = 2:20
+        statPlot.AxesProperties(i).LegendVisible = 'off';
+   end
+end   
+
