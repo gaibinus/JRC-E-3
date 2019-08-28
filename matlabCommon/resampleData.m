36924c47e921c5d9ef63276d5aefb34fa3328e0d
Filip Geib <filip9geib@gmail.com>
2019-08-28 11:03:29 +0200

    returning table option added

---
+++ matlabCommon/resampleData.m
@@ -0,0 +1,27 @@
+function ret = resampleData(pathIn, pathOut, frequency, mode)
+
+% load data to time table
+data = data2timetable(pathIn);
+	
+% resample time table to desired frequency with method
+data = retime(data, 'regular', mode, 'SampleRate', frequency);
+
+% change timetable to table
+data = timetable2table(data);
+
+% change to table and exclude ' sec' from time column
+data = convertvars(data,'Time','seconds');
+
+% export table to new file or return as table
+if strcmp(pathout,'return') == true
+    % return resampled data as table
+    ret = data;
+else
+    % export table to desired csv file
+    writetable(data, pathOut);
+
+    % if okay, return true
+    ret = true;
+end
+
+end
