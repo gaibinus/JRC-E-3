d4276ecbda77b147f85dcf912d95a9393217bb63
Filip Geib <filip9geib@gmail.com>
2019-08-29 15:17:10 +0200

    means to config added

--- matlabScripts/compensateGravity.m
+++ matlabScripts/compensateGravity.m
@@ -18,9 +18,12 @@ quat = py.functions.readConfig(pathConfig, 'quat_mean');
 quatI = py.functions.readConfig(pathConfig, 'quat_mean_i');
 quatJ = py.functions.readConfig(pathConfig, 'quat_mean_j');
 quatK = py.functions.readConfig(pathConfig, 'quat_mean_k');
+freq = py.functions.readConfig(pathConfig, 'sample_rate');
+bnwStart = py.functions.readConfig(pathConfig, 'bnw_start');
+bnwEnd = py.functions.readConfig(pathConfig, 'bnw_stop');
 
 % check if read values contains any 'nan'
-if sum(isnan([gravity quat quatI quatJ quatK])) > 0
+if sum(isnan([gravity quat quatI quatJ quatK freq bnwStart bnwEnd])) > 0
    error("Uncomplete config file"); 
 end
 
@@ -46,6 +49,42 @@ data{:,'AccZ'} = dataComputed(:,3);
 
 % write table to CSV
 writetable(data, pathOut);
+
+%% COMPUTE MEANS IN BNW AND WRITE THEM TO CONFIG FILE
+
+% change bnw time to line number
+bnwStart = bnwStart * freq;
+bnwEnd = bnwEnd * freq;
+
+% compute means of every variable in data table
+accMean(1) = mean(data{bnwStart : bnwEnd, 'AccX'});
+accMean(2) = mean(data{bnwStart : bnwEnd, 'AccY'});
+accMean(3) = mean(data{bnwStart : bnwEnd, 'AccZ'});
+
+gyrMean(1) = mean(data{bnwStart : bnwEnd, 'GyrX'});
+gyrMean(2) = mean(data{bnwStart : bnwEnd, 'GyrY'});
+gyrMean(3) = mean(data{bnwStart : bnwEnd, 'GyrZ'});
+
+magMean(1) = mean(data{bnwStart : bnwEnd, 'MagX'});
+magMean(2) = mean(data{bnwStart : bnwEnd, 'MagY'});
+magMean(3) = mean(data{bnwStart : bnwEnd, 'MagZ'});
+
+pressMean = nanmean(data{bnwStart : bnwEnd, 'Pres'});
+
+% write computed means into config file
+py.functions.writeConfig(pathConfig, 'acc_mean_x', accMean(1));
+py.functions.writeConfig(pathConfig, 'acc_mean_y', accMean(2));
+py.functions.writeConfig(pathConfig, 'acc_mean_z', accMean(3));
+
+py.functions.writeConfig(pathConfig, 'gyr_mean_x', gyrMean(1));
+py.functions.writeConfig(pathConfig, 'gyr_mean_y', gyrMean(2));
+py.functions.writeConfig(pathConfig, 'gyr_mean_z', gyrMean(3));
+
+py.functions.writeConfig(pathConfig, 'mag_mean_x', magMean(1));
+py.functions.writeConfig(pathConfig, 'mag_mean_y', magMean(2));
+py.functions.writeConfig(pathConfig, 'mag_mean_z', magMean(3));
+
+py.functions.writeConfig(pathConfig, 'pres_mean', pressMean);
 
 %% END OF SCRIPT
 ret = true;
