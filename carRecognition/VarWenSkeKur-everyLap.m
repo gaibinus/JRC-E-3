afd53719447e3866e9f84fb7f4d54345e37350f5
Filip Geib <filip9geib@gmail.com>
2019-08-29 11:33:52 +0200

    renamed

--- carRecognition/VarWenSkeKur-everyLap.m
+++ carRecognition/VarWenSkeKur-everyLap.m
@@ -10,9 +10,8 @@ load('C:\Users\geibfil\Desktop\JRC-E-3\experiments\data_10.mat', 'data');
 SEGMENT_SIZE = 20; % in [s]
 SAMPLE_RATE = 10; % in [Hz]
 CARS = size(data, 1);
-LAPS = size(data, 2);
 
-%% COMPUTE AND PLOT VARIANCE
+%% COMPUTE STATISTICS
 
 % compute increase between segments
 segIncrease = SEGMENT_SIZE * SAMPLE_RATE;
@@ -28,25 +27,24 @@ results = cell(CARS, 8);
 
 for car = 1 : CARS
     % create table header as cell array
-    header = sprintf('Car%dLap%d,', [car*ones(1,LAPS) ; 1:LAPS]);
+    header = sprintf('Car%dLap%d,', [car*ones(1,20) ; 1:20]);
     header = strsplit(header, ',');
     header(cellfun('isempty',header)) = [];
     
     % create tables of every method for current car
     for method = 1 : 8
         results{car, method} = ...
-            array2table(zeros(segCount, LAPS), 'VariableNames', header);
+            array2table(zeros(segCount, 20), 'VariableNames', header);
     end
-    % results{car, :} = array2table(zeros(segCount, LAPS), 'VariableNames', header);
        
     for segment = 1 : segCount
         % compute end of current segment 
         segEnd = segIncrease * segment;
         
         % prealloc tmp results as: tmpResults{method, lap}
-        tmpResults = NaN(8, LAPS);
+        tmpResults = NaN(8, 20);
               
-        for lap = 1 : LAPS    
+        for lap = 1 : 20    
             % check if segEnd overflov current lap
             if segEnd > size(data{car, lap}, 1)
                 continue;
@@ -73,21 +71,20 @@ for car = 1 : CARS
         for method = 1 : 8
             results{car, method}{segment, :} = tmpResults(method,:);            
         end
-        % results{car, :}{segment, :} = tmpResults(:,:); 
         
     end
 end
+
+%% PLOT STATISTICS RESULTS FOR EVERY METHODE
 
 % create array with method names and data names
 methodNames = ["variance", "wentropy", "skewness", "kurtosis"];
 dataNames = ["acceleration on Z axis", "gyroscope on Y axis"];
 
-%% PLOT VARIANCE RESULTS FOR EVERY METHODE
-
-for method = 1 : 1
+for method = 1 : 8
 
    % compute methode and data name strings
-   methodStr = methodNames(rem(5-1, 4) + 1);
+   methodStr = methodNames(rem(method-1, 4) + 1);
    dataStr = dataNames(floor((method-1)/4) + 1);
 
    % create figure and set it up
@@ -95,15 +92,11 @@ for method = 1 : 1
    fig.Name = strcat(methodStr, ' of ', dataStr);
    
    % join all cars to one table
-   plotResults = table();
-   for car = 1 : CARS
-       plotResults = [plotResults, results{car, method}];
-   end
-   % plotResults = table(horzcat(results{:,method}));
+   plotResults = [results{:,method}];
    
    % create display variables parameter
-   dispVar = cell(LAPS,1);
-   for lap = 1 : LAPS
+   dispVar = cell(20,1);
+   for lap = 1 : 20
        tmp = ...
            strsplit(sprintf('Car%dLap%d\n', [1:CARS; lap * ones(1,CARS)]));
        tmp(cellfun('isempty',tmp)) = [];
@@ -111,22 +104,42 @@ for method = 1 : 1
    end
    
    % create display labels parameter
-   dispLab = strsplit(sprintf('Lap %d,', 1:LAPS), ',');
+   dispLab = strsplit(sprintf('Lap %d,', 1:20), ',');
    dispLab(cellfun('isempty',dispLab)) = [];
    
    % create legend labels parameter
    legLab = strsplit(sprintf('Car %d,', 1:CARS), ',');
    legLab(cellfun('isempty',legLab)) = [];
    
-   % create stacked plot and set it up
-   statPlot = stackedplot(plotResults, dispVar);
-   statPlot.DisplayLabels = dispLab;
-   statPlot.XLabel = 'Segment ID';
-   statPlot.AxesProperties(1).LegendLabels = legLab;
-   statPlot.AxesProperties(1).LegendVisible = 'on';
+   % left side of figure
+   subLeft = subplot(1,2,1);
    
-   for i = 2:LAPS
-        statPlot.AxesProperties(i).LegendVisible = 'off';
+   % create left plot from 1 to 10
+   leftPlot = stackedplot(plotResults, dispVar(1:10));
+   
+   leftPlot.Units = 'normalized';
+   leftPlot.ActivePositionProperty  = 'position';
+   
+   leftPlot.DisplayLabels = dispLab(1:10);
+   leftPlot.XLabel = 'Segment ID';
+   leftPlot.AxesProperties(1).LegendLabels = legLab;
+   leftPlot.AxesProperties(1).LegendVisible = 'on'; 
+   for i = 2:10
+        leftPlot.AxesProperties(i).LegendVisible = 'off';
    end
+   
+   % right side of figure
+   subRight = subplot(1,2,2);
+   
+   % create left plot from 11 to 20
+   rightPlot = stackedplot(plotResults, dispVar(11:20));
+   rightPlot.DisplayLabels = dispLab(11:20);
+   rightPlot.XLabel = 'Segment ID';
+   rightPlot.AxesProperties(1).LegendLabels = legLab;
+   rightPlot.AxesProperties(1).LegendVisible = 'on'; 
+   for i = 2:10
+        rightPlot.AxesProperties(i).LegendVisible = 'off';
+   end
+      
 end   
 
