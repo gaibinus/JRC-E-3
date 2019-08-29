69c6dedcd330d32fc285adb092fe886ac6a1b89e
Filip Geib <filip9geib@gmail.com>
2019-08-29 15:52:25 +0200

    mean in bnw table finished

--- carRecognition/createDataStructure.m
+++ carRecognition/createDataStructure.m
@@ -1,5 +1,17 @@
 function ret = createDataStructure(pathExp, resampleRate)
 
+%% PYTHON IN MATLAB WORKAROUND
+
+% load folder with python scripts
+pathPython = strrep(pwd, 'matlabScripts', 'pythonScripts');
+py_addpath(pathPython);
+
+% import python module and reload it
+pyModule = py.importlib.import_module('functions');
+py.importlib.reload(pyModule);
+
+%% MAIN DATA STRUCTURE CREATION
+
 % load experiment configuration file
 config = readtable(strcat(pathExp, '\config.csv'));
 
@@ -37,10 +49,12 @@ for car = 1 : CARS
     end
 end
 
+%% BNW MEAN STRUCTURE CREATION
+
 % create BNW mean table header
 header = data{1, 1}.Properties.VariableNames;
 header(strcmp(header,'Time')) = [];
-header = [{'CarNo' 'ExpName'} , header];
+header = [{'CarNo'} , header];
  
 % create BNW mean table CARS x VALUES
 bnw = array2table(zeros(CARS, size(header, 2)), 'VariableNames', header);
@@ -51,10 +65,24 @@ for car = 1 : CARS
     pathConfig = ...
         char(strcat(pathExp, '\', config{car, 'ExpName'},'\config.txt'));
     
+    % fill BNW table with current car info
+    bnw{car, 'CarNo'} = car;
     
+    % load needed data from config file to BNW table
+    bnw{car, 'AccX'} = py.functions.readConfig(pathConfig, 'acc_mean_x');
+    bnw{car, 'AccY'} = py.functions.readConfig(pathConfig, 'acc_mean_y');
+    bnw{car, 'AccZ'} = py.functions.readConfig(pathConfig, 'acc_mean_z');
     
+    bnw{car, 'GyrX'} = py.functions.readConfig(pathConfig, 'gyr_mean_x');
+    bnw{car, 'GyrY'} = py.functions.readConfig(pathConfig, 'gyr_mean_y');
+    bnw{car, 'GyrZ'} = py.functions.readConfig(pathConfig, 'gyr_mean_z');
     
+    bnw{car, 'MagX'} = py.functions.readConfig(pathConfig, 'mag_mean_x');
+    bnw{car, 'MagY'} = py.functions.readConfig(pathConfig, 'mag_mean_y');
+    bnw{car, 'MagZ'} = py.functions.readConfig(pathConfig, 'mag_mean_z');
 end
+
+%% CREATED STRUCTURES SAVING
 
 % compute save patch
 if exist('resampleRate','var')
@@ -67,7 +95,7 @@ end
 fprintf('INFO: saving data to:\n%s\n', pathSave);
 
 % save data to disc
-save(pathSave, 'data');
+save(pathSave, 'data', 'bnw');
 
 % if okay, return true
 ret = true;
